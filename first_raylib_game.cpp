a6eeaf3657918ac2822f280fbfa3abab88362424
Omar Ibrahim <omaribrahimsaad@gmail.com>
2024-10-26 16:45:32 +0800

    first commit

---
+++ first_raylib_game.cpp
@@ -0,0 +1,12 @@
+﻿// first_raylib_game.cpp : Defines the entry point for the application.
+//
+
+#include "first_raylib_game.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
